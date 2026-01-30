81baa88b312553d8cd2d43718cb3b023f78fcb43
kanishk0terabyte <k6609502@gmail.com>
2026-01-30 22:40:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Chemverse/',   // ⚠️ Repo name SAME hona chahiye
+  base: '/',   // ⚠️ Repo name SAME hona chahiye
 })
